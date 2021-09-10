4a190dc482d0a8af7a6267d0a72e64526d132ade
MyMac <mymac@dhcp-wifi-8021x-155-41-79-67.bu.edu>
2021-09-10 12:38:44 -0400

    Basic functionalities for scanning or manually entering barcode

--- calorie-scanner/Components/ManualBarcodeScreen.js
+++ calorie-scanner/Components/ManualBarcodeScreen.js
@@ -1,9 +1,26 @@
-import * as React from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import { Button, SafeAreaView, Text, TextInput } from 'react-native';
 
-export default function ManualBarcodeScreen({ navigation })
-{
+export default function ManualBarcodeScreen({ navigation }) {
+  const [barcode, setBarcode] = React.useState("");
+
+  const searchBarcode = () => {
+    console.log('barcode', barcode)
+    navigation.navigate('ResultScreen');
+    // TODO: Call backend function and show results on ResultScreen
+  }
+
   return (
-    <Text>Type in the barcode</Text>
+    <SafeAreaView>
+      <Text>Enter Barcode</Text>
+      <TextInput
+        onChangeText={setBarcode}
+        placeholder="00000000000"
+      />
+      <Button
+        title="Search Barcode"
+        onPress={searchBarcode}
+      />
+    </SafeAreaView>
   );
 }
--- calorie-scanner/Components/ScannerScreen.js
+++ calorie-scanner/Components/ScannerScreen.js
@@ -1,9 +1,58 @@
-import * as React from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import { Text, View, Dimensions } from 'react-native';
+import { Camera } from 'expo-camera';
 
-export default function ScannerScreen({ navigation })
-{
-  return (
-    <Text>Scanner Screen</Text>
-  );
+export default class CameraScreen extends React.Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      hasPermission: null,
+      cameraType: Camera.Constants.Type.back,
+      barcode: ""
+    };
+  }
+
+  async componentDidMount()
+  {
+    this.getPermissionAsync();
+    this.setState({ barcode: "" });
+  }
+
+  getPermissionAsync = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    console.log('status', status);
+    this.setState({ hasPermission: status == 'granted' });
+  }
+
+  searchBarcode = (barcode) => {
+    this.setState({ barcode });
+    console.log('barcode', barcode)
+    this.props.navigation.navigate('ResultScreen');
+    // TODO: Call backend function and show results on ResultScreen
+  }
+
+  render() {
+    const { hasPermission, cameraType } = this.state;
+    const screenHeight = Dimensions.get('window').height;
+
+    if (hasPermission === null) {
+      console.log('permission null');
+      return <Text>This app needs access to your camera. Reload the app if you gave the app permission to use the camera.</Text>;
+    }
+
+    if (hasPermission === false) {
+      return <Text>This app needs access to your camera. Reload the app if you gave the app permission to use the camera.</Text>;
+    }
+
+    return (
+      <Camera
+        type={cameraType}
+        style={{height: screenHeight}}
+        ref={ref => {this.camera = ref}}
+        onBarCodeScanned={(res) => this.state.barcode ? null : this.searchBarcode(res.data)}
+      />
+    );
+  }
 }
